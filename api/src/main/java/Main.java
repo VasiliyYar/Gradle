e1069f0acb58ef03833cd47da4deff9f1f7bbb5e
Vasiliy Solovev <vasya.79@bk.ru>
2023-01-20 22:41:33 +0300

    add src

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         MyService myService = new MyService();
         System.out.println(myService.getMyEntity());
         System.out.println(myService.setMyEntity(new MyEntity("second")));
